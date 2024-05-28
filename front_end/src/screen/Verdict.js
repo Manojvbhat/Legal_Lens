94545c0d62f984217406f66ca2452d0289ca9851
Manojvbhat <manojvbhat2002@gmail.com>
2024-05-28 18:27:56 +0530

    Basic page for verdict generation
    Add 10 seconds delay before showing the verdict.

--- front_end/src/screen/Verdict.js
+++ front_end/src/screen/Verdict.js
@@ -4,16 +4,19 @@ import { Comment } from "react-loader-spinner";
 export default function Verdict()
 {
     const [time,settime] = useState(5)
-    useEffect(()=>
-    {
-
-        setTimeout(settime(0), 10000)
-    }
-   ,[time] )
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          settime(0);
+        }, 10000);
+    
+        // Clear the timeout if the component is unmounted to avoid memory leaks
+        return () => clearTimeout(timer);
+      }, []);
+    
 
     return(
     <div>
-           { time} ? <Comment
+           { time===5 ? <Comment
         visible={true}
         height="200"
         width="200"
@@ -27,7 +30,7 @@ export default function Verdict()
     <div>
         <p className=" text-7xl font-extrabold pt-5"> Verdict</p>
 			<textarea className=" p-8 w-full text-black h-56 text-justify overflow-auto mt-20 rounded-2xl" type="text" value="Masala Puri" />
-    </div>
+    </div>}
     </div>
     )
 }
